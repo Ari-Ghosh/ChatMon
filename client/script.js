8037ef173e565a092be74dde9de265a56e5bdd49
Arijit Ghosh <arijitghosh120403@gmail.com>
2022-12-31 08:32:29 +0530

    Updated and improved frontend

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5100', {
+    const response = await fetch('https://chatmon.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
